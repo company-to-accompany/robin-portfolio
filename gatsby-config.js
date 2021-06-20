9a562a692d89c1198bdf95abb50a0f128ca460e1
Robin <robinbtech17@gmail.com>
2021-06-20 19:16:05 +0530

    Favicon manifest added.

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,19 @@
 
 module.exports = {
   pathPrefix: "/robin-portfolio",
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Robin Portfolio`,
+        short_name: `Robin Portfolio`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#343434`,
+        display: 'standalone',
+        icon: 'src/assets/img/favicon.png',
+      },
+    }
+  ],
 }
