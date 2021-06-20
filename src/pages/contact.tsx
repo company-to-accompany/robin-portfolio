4f5b55d8d60742ac999f935ff3d0a87e9aa33d8f
Robin <robinbtech17@gmail.com>
2021-06-20 17:51:22 +0530

    Contact page style implementation.

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,30 +1,57 @@
 import React from "react"
 import Layout from "../components/Layout"
 import { Title } from "../components/Title"
+import "../assets/styles/contact.css"
 
 const Contact = () => {
+  const handleSubmit = event => {
+    event.preventDefault()
+  }
   return (
     <Layout>
       <Title text="Robin | Contact" />
       <section id="contact">
-        <article>
-          <h2>Have questions? Need to hire me?</h2>
+        <article className="contact__container">
+          <h2 className="contact__title">Have questions? Need to hire me?</h2>
           <p>Submit the form and I'll reach you</p>
-          <form>
-            <div>
-              <p>01</p>
-              <input type="text" name="name" id="name" />
+          <form className="contact__form" onSubmit={handleSubmit}>
+            <div className="contact__input-container">
+              <span>01</span>
+              <input
+                autoComplete="off"
+                className="contact__form-input"
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Type your name..."
+              />
             </div>
-            <div>
-              <p>02</p>
-              <input type="email" name="email" id="email" />
+            <div className="contact__input-container">
+              <span>02</span>
+              <input
+                autoComplete="off"
+                className="contact__form-input"
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Type your email..."
+              />
             </div>
-            <div>
-              <p>03</p>
-              <input type="text" name="message" id="message" />
+            <div className="contact__input-container">
+              <span>03</span>
+              <input
+                autoComplete="off"
+                className="contact__form-input"
+                type="text"
+                name="message"
+                id="message"
+                placeholder="Type your message..."
+              />
             </div>
-            <div>
-              <button type="submit">Submit</button>
+            <div className="contact_btn-container">
+              <button className="btn" type="submit">
+                Submit
+              </button>
             </div>
           </form>
         </article>
