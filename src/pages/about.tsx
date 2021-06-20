6a3097b312b0fd274040b06e5b7c0fad81ea50e2
Robin <robinbtech17@gmail.com>
2021-06-20 09:38:07 +0530

    About us page style implementation.

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,20 +2,41 @@ import { Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
 import { Title } from "../components/Title"
+// @ts-ignore
+import image from "../assets/img/robin.jpg"
+
+import "../assets/styles/about.css"
 
 const About = () => {
   return (
     <Layout>
       <Title text="Robin | About" />
       <section id="about">
-        <article>
-          <figure>
-            <img src="" alt="Robin Baskaran" />
-          </figure>
-          <div>
-            <h2>About me</h2>
-            <p>description</p>
-            <Link to="/contact">Contact me</Link>
+        <article className="aboutme">
+          <div className="aboutme__item aboutme__left">
+            <figure className="aboutme__img-container">
+              <img src={image} alt="Robin Baskaran" />
+            </figure>
+          </div>
+          <div className="aboutme__item aboutme__right">
+            <h2 className="aboutme__title">About me</h2>
+            <p className="aboutme__description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+              doloremque necessitatibus esse placeat dolores, corrupti unde
+              quidem est rem quo deleniti nam. Incidunt dolorum consequatur
+              natus rerum perferendis sapiente assumenda quidem ab beatae
+              tempore deleniti quos, porro dolore est, nihil quaerat facere
+              numquam fugit dolor, odio illum mollitia? Non culpa illo ducimus
+              consectetur dolorem eaque dolorum recusandae. Ea vitae libero
+              numquam sed maxime! Nulla odit ut placeat, iure, consectetur,
+              officiis non inventore debitis cum optio consequuntur quod
+              expedita ea at. Accusantium, id? Nisi deleniti quia corrupti
+              voluptate, accusantium fuga quod eligendi voluptatem facere
+              consectetur quos sit explicabo officia eaque excepturi.
+            </p>
+            <Link className="btn" to="/contact">
+              Contact me
+            </Link>
           </div>
         </article>
         <article>
